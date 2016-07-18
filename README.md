77774f32748f9abd4e044a0d089d8cbae80cecbb
Brian Bister <brianbister@gmail.com>
2016-07-17 20:18:06 -0700

    Lots of changes.

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ## To start
 
+- Install Node https://nodejs.org/
+
+- Run `npm install`
+
 - Run `node server.js`
+
+- Direct your browser to localhost:8000
--- js/app.js
+++ js/app.js
@@ -21,8 +21,23 @@ function ViewModel() {
     new Place("The Woolworth Building", 40.71241, -74.00839, "1913 neo-Gothic skyscraper, once the world's tallest building."),
     new Place("9/11 Memorial", 40.71140, -74.01251, "Plaza, pools & exhibits honoring victims of 1993 & 2001 WTC terrorist attacks.")
   ]);
-  self.articles = ko.observableArray([]);
+  self.allPlaces = self.places();
   self.filter = ko.observable("");
+  self.focusedPlace = ko.observable();
+  self.articles = ko.observableArray([]);
+  self.error = ko.observable("");
+
+  self.filterPlaces = function() {
+    self.places([]);
+    for (var i = 0; i < self.allPlaces.length; i++) {
+      if (self.allPlaces[i].name.toLowerCase().indexOf(self.filter().toLowerCase()) > -1) {
+        self.places.push(self.allPlaces[i]);
+      } else {
+        self.allPlaces[i].marker.setMap(null);
+      }
+    }
+    self.replaceMarkers();
+  };
 
   self.initMap = function() {
     var mapDiv = document.getElementById('map');
@@ -37,55 +52,57 @@ function ViewModel() {
     self.replaceMarkers();
   };
 
-  self.filterPlaces = function() {
-    var allPlaces = self.places();
-    var filteredPlaces = [];
-    for (var i = 0; i < allPlaces.length; i++) {
-      if (!self.filter() || allPlaces[i].name.indexOf(self.filter()) != -1) {
-        filteredPlaces.push(allPlaces[i]);
-      } else {
-        // Remove marker
-        allPlaces[i].marker.setMap(null);
-      }
+  self.focusPlace = function(place) {
+    self.focusedPlace(place);
+    for (var i = 0; i < self.places().length; i++) {
+      self.places()[i].marker.setAnimation(null);
     }
-    self.places(filteredPlaces);
-    self.replaceMarkers();
+    place.marker.setAnimation(google.maps.Animation.BOUNCE);
+    map.setCenter(place.marker.getPosition());
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "bebe9ce324f34ca5b7f48a4e5f6db6bd",
+      'q': place.name,
+      'sort': "newest"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      var docs = result.response.docs;
+      self.articles([]);
+      for (var j = 0; j < docs.length && j < 2; j++) {
+        self.articles.push(new Article(docs[j].headline.main, docs[j].snippet));
+      }
+    }).fail(function(err) {
+      self.error("An error occurred loading your articles.");
+    });
   };
 
   self.replaceMarkers = function() {
-    var allPlaces = self.places();
+    for (var i = 0; i < self.allPlaces.length; i++) {
+      if (self.allPlaces[i].marker) {
+        self.allPlaces[i].marker.setMap(null);
+      }
+    }
 
     // Add new markers
-    for (i = 0; i < allPlaces.length; i++) {
-      var marker = new google.maps.Marker({
-        map: map,
-        position: allPlaces[i],
-        title: allPlaces[i].name
-      });
-      marker.name = allPlaces[i].name;
-      marker.addListener("click", function() {
-        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({
-          'api-key': "bebe9ce324f34ca5b7f48a4e5f6db6bd",
-          'q': this.name,
-          'sort': "newest"
-        });
-        $.ajax({
-          url: url,
-          method: 'GET',
-        }).done(function(result) {
-          var docs = result.response.docs;
-          self.articles([]);
-          for (var j = 0; j < docs.length && j < 2; j++) {
-            self.articles.push(new Article(docs[j].headline.main, docs[j].snippet));
-          }
-        }).fail(function(err) {
-          throw err;
-        });
-      });
-      allPlaces[i].marker = marker;
+    for (var i = 0; i < self.places().length; i++) {
+      self.replaceMarker(self.places()[i]);
     }
   };
+
+  self.replaceMarker = function(place) {
+    place.marker = new google.maps.Marker({
+      map: map,
+      position: place,
+      title: place.name
+    });
+    place.marker.name = place.name;
+    place.marker.addListener("click", function() {
+      self.focusPlace(place);
+    });
+  }
 
   self.initMap();
 }
